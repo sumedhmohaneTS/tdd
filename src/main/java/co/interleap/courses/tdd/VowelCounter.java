20b59e9931c798cb9268949fa94af6ad7f47868e
sumedhmohaneTS <sumedh.mohane@townscript.com>
2019-12-05 12:44:42 +0530

    test cases added

--- src/main/java/co/interleap/courses/tdd/VowelCounter.java
+++ src/main/java/co/interleap/courses/tdd/VowelCounter.java
@@ -1,4 +1,8 @@
 package co.interleap.courses.tdd;
 
 public class VowelCounter {
+
+  public Integer getCount(String input) {
+    return input.length();
+  }
 }
--- src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
+++ src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
@@ -1,9 +1,25 @@
 package co.interleap.courses.tdd;
 
+
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class VowelCounterTest {
 
-    @Test
-    public void dummy(){}
+  @Test
+  public void testEmptyString() {
+    assertEquals(Integer.valueOf(0), new VowelCounter().getCount(""));
+  }
+
+  @Test
+  public void testSingleVowel() {
+    assertEquals(Integer.valueOf(0), new VowelCounter().getCount("a"));
+  }
+
+  @Test
+  public void testMultipleVowel() {
+    assertEquals(Integer.valueOf(0), new VowelCounter().getCount("aeiou"));
+  }
+
+
 }
